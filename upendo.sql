3b9d67fcbdea061c04415b7bf4ac86cb6786c315
Amadou Sarjo Jallow <asj.sarjo@gmail.com>
2021-11-18 18:38:06 +0100

    Initial sql file

---
+++ upendo.sql
@@ -0,0 +1,26 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 13.4
+-- Dumped by pg_dump version 13.4
+
+-- Started on 2021-11-18 18:34:39
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+-- Completed on 2021-11-18 18:34:40
+
+--
+-- PostgreSQL database dump complete
+--
+
