b8ea6f5b9dbd2a4cd8b31a5cb8ceb26e91f64fad
Minigrim0 <grimauflorent@gmail.com>
2021-12-22 10:26:31 +0100

    [Upd] started updating the database

--- DeliveryServices/grimau-florent-database.sql
+++ DeliveryServices/grimau-florent-database.sql
@@ -10,39 +10,26 @@ CREATE DATABASE grimauflorentois
 
 BEGIN;
 
-CREATE TABLE delivery_status_description (
-    delivery_status character varying(128) NOT NULL,
-    decription text NOT NULL
-);
-
-CREATE TABLE delivery_services_types (
-    delivery_kind character varying(128) NOT NULL,
-    price double precision NOT NULL,
-    delivery_duration integer NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Warehouse(
+CREATE TABLE IF NOT EXISTS warehouse(
     ID              character varying(255) NOT NULL PRIMARY KEY,
     name            CHAR(50) NOT NULL,
     location        CHAR(50)
 );
 
-CREATE TABLE IF NOT EXISTS TransportCompany(
+CREATE TABLE IF NOT EXISTS transportcompany(
     name            CHAR(50) NOT NULL UNIQUE PRIMARY KEY,
     price_per_km    DOUBLE DEFAULT 0,
     api_key         CHAR(50) DEFAULT 'missing'
 );
 
-CREATE TABLE IF NOT EXISTS TransportVehicle(
+CREATE TABLE IF NOT EXISTS transportvehicle(
     ID              character varying(255) NOT NULL PRIMARY KEY,
     position        CHAR(50),
 
     company         CHAR(50) NOT NULL
 );
 
-ALTER TABLE TransportVehicle ADD FOREIGN KEY(company) REFERENCES TransportCompany(name) ON DELETE CASCADE;
-
-CREATE TABLE IF NOT EXISTS DeliveryTrip(
+CREATE TABLE IF NOT EXISTS deliverytrip(
     ID                  character varying(255) NOT NULL PRIMARY KEY,
     departure_time      TIMESTAMP,
     arrival_time        TIMESTAMP,
@@ -51,10 +38,7 @@ CREATE TABLE IF NOT EXISTS DeliveryTrip(
     starting_warehouse  character varying(255) NOT NULL
 );
 
-ALTER TABLE DeliveryTrip ADD FOREIGN KEY(truck) REFERENCES TransportVehicle(ID) ON DELETE CASCADE;
-ALTER TABLE DeliveryTrip ADD FOREIGN KEY(starting_warehouse) REFERENCES Warehouse(ID) ON DELETE CASCADE;
-
-CREATE TABLE IF NOT EXISTS Orders(
+CREATE TABLE IF NOT EXISTS order(
     ID                  character varying(255) NOT NULL PRIMARY KEY,
     email               CHAR(100),
     date                DATETIME NOT NULL,
@@ -64,6 +48,22 @@ CREATE TABLE IF NOT EXISTS Orders(
     delivery_type       character varying(128) NOT NULL,
     trip                character varying(255),
 );
+
+CREATE TABLE public."order" (
+    orderid character varying(15) NOT NULL,
+    orderstatus character varying(128) NOT NULL,
+    orderdate timestamp without time zone NOT NULL,
+    customeremail character varying(256) NOT NULL,
+    customeraddress character varying(512) NOT NULL,
+    orderamount numeric(10,2) NOT NULL,
+    deliveryservice character varying(128) NOT NULL
+);
+
+
+ALTER TABLE TransportVehicle ADD FOREIGN KEY(company) REFERENCES TransportCompany(name) ON DELETE CASCADE;
+
+ALTER TABLE DeliveryTrip ADD FOREIGN KEY(truck) REFERENCES TransportVehicle(ID) ON DELETE CASCADE;
+ALTER TABLE DeliveryTrip ADD FOREIGN KEY(starting_warehouse) REFERENCES Warehouse(ID) ON DELETE CASCADE;
 
 ALTER TABLE Orders ADD FOREIGN KEY(trip) REFERENCES DeliveryTrip(ID) ON DELETE CASCADE;
 ALTER TABLE Orders ADD FOREIGN KEY(delivery_type) REFERENCES delivery_services_types(delivery_kind) ON DELETE CASCADE;
